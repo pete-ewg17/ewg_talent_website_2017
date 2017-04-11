a640be6cb5a0a51af6b60aaf0f2f61743cf5918e
pete-ewg17 <peteakhurst-ewg@Petes-MacBook-Pro.local>
2017-04-11 14:32:54 +1000

    Added Repo

---
+++ README.md
@@ -0,0 +1,33 @@
+# ewg_tech_website_2017
+
+Form Values 
+
+Contact Form - EWG Tech 
+
+General contact form no check boxes or select options are included. 
+
+
+
+Request A Demo Form - EWG Tech 
+
+Type of Event or Organisation?* - Required field 
+
+value="national"
+value="sporting"
+value="private"
+value="charity"
+value="other"
+
+
+Within the next 12 months how many events would you like our solution to help you with? - Required field 
+
+<option value="0">0</option>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6+</option>
+<option vales="7">Other</option>
+
+
---
+++ js/script.js
@@ -0,0 +1,141 @@
+/*===========
+  BACK TO TOP 
+=============*/
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+	// scroll body to 0px on click
+	$('#back-to-top').click(function () {
+		$('#back-to-top').tooltip('hide');
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+});
+
+/*===========
+  BACK TO TOP 
+=============*/
+
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#register').fadeIn();
+		} else {
+			$('#register').fadeOut();
+		}
+	});
+});
+
+
+
+/*=================
+  SMOOTH SCROLL
+=================*/
+$(document).ready(function () {
+	// Add smooth scrolling to all links
+	$("a").on('click', function (event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function () {
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
+});
+
+/*=================
+  ANIMATE ON SCROLL
+=================*/
+AOS.init({
+	duration: 1200
+});
+AOS.init({
+	disable: 'mobile'
+});
+
+/*===========
+  FORM 
+=============*/
+
+$(function () {
+
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "technology/contact.php";
+            var f = $(this).serializeArray();
+            $("#loading_div").css('display', 'block');
+            $("#submitButton").css('display', 'none');
+            $.post(url, {
+                fields:f
+            }, function (result) {
+                $("#loading_div").css('display', 'none');
+                $("#submitButton").css('display', 'block');
+                $("form").trigger("reset"); 
+                $("#myModal").modal('hide');
+                $("#contact-form").reset();
+                    alert("Thank you");
+                
+            });
+
+            return false;
+        }
+    })
+});
+
+/*===========
+  Counter 
+=============*/
+$(document).ready(function () {
+	$('.counter-num').counterUp({
+		delay: 10,
+		time: 1000
+	});
+});
+
+/*===========
+  Client Logo Carousel 
+=============*/
+$(document).ready(function () {
+	$('.customer-logos').slick({
+		slidesToShow: 4,
+		slidesToScroll: 2,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: true,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3
+			}
+        }, {
+			breakpoint: 520,
+			settings: {
+				slidesToShow: 2
+			}
+        }]
+	});
+});
