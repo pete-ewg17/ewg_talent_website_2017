c9048f783dfa56ec6c120d878bd3f2681ce9797b
chrisgrant <chris.grant@grantet.com.au>
2017-05-02 11:27:07 +1000

    remove conflicting code

--- get-started-1.php
+++ get-started-1.php
@@ -107,7 +107,6 @@ $_SESSION['question_1'] = $_POST;
  
  
  <div class="content-section-a">
-<<<<<<< HEAD
   		<form id="final_ans" method="post" class='wizard_form' role="form" action='thankyou'>
               <div class="messages"></div>
               <div class="controls">
@@ -192,92 +191,6 @@ $_SESSION['question_1'] = $_POST;
                   </div>
                   <div class="col-md-6 col-md-offset-2 submit">
                     <a  class="btn btn-success btn-send disabled" id="next">Create My Portal</a >
-=======
-  <form id="final_ans" method="post" class='wizard_form' role="form" action='thankyou'>
-    <div class="messages"></div>
-    <div class="controls">
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <div class="form-group" data-toggle="validator" role="form">
-            <label for="form_name">Name*</label>
-            <input id="contact_name" type="text" name="name" class="form-control mandatory" placeholder="Please enter your full name" required="required" data-error="Name is required.">
-            <div class="help-block with-errors"></div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <div class="form-group">
-            <label for="form_name">Email*</label>
-            <input id="contact_email" type="email" name="email" class="form-control mandatory" placeholder="Please enter your email">
-            <div class="help-block with-errors"></div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <div class="form-group">
-            <label for="form_phone">Phone*</label>
-            <input id="contact_phone" type="tel" name="phone" class="form-control mandatory" placeholder="Please enter your phone number" required="required">
-            <div class="help-block with-errors"></div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <div class="form-group">
-            <label for="form_company">Company*</label>
-            <input id="company" type="text" name="company" class="form-control mandatory" placeholder="Please enter your Company or Organization" required="required">
-            <div class="help-block with-errors"></div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <div class="form-group">
-            <label for="form_company">Position/Title*</label>
-            <input id="position" type="text" name="position" class="form-control mandatory" placeholder="Please enter your position in the company" required="required">
-            <div class="help-block with-errors"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <div class="form-group">
-          <label for="sel1">Address</label>
-          <input type='text' name="address" id='address' class='form-control mandatory' />
-          <div class="help-block with-errors"></div>
-          <input type='hidden' data-st='long_name' id='street_number' name='street_number' value='' />
-          <input type='hidden' data-st='long_name' id='route' name='route' value='' />
-          <input type='hidden' data-st='long_name' id='locality' name='locality' value='' />
-          <input type='hidden' data-st='long_name' id='administrative_area_level_1' name='administrative_area_level_1' value='' />
-          <input type='hidden' data-st='long_name' id='country' name='country' value='' />
-          <input type='hidden' data-st='long_name' id='postal_code' name='postal_code' value='' />
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <div class="form-group">
-          <label for="sel1">Password</label>
-          <input type='password' name="password" id='password' class='form-control mandatory' />
-          <div class="help-block with-errors"></div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <div class="form-group">
-          <label for="sel1">Confirm Password</label>
-          <input type='password' id='password2' class='form-control mandatory' />
-          <div class="help-block with-errors"></div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6 col-md-offset-2 submit">
-      <a class="btn btn-success btn-send disabled" id="next">Send Message</a >
->>>>>>> origin/master
                     <br>
                     <input type="hidden" id="form_type" name="form_type" value="new_talent">
                     <input type="hidden" id="email_template" name="email_template" value="00_ewg-talent-template">
