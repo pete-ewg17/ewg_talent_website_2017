5300dea259a6faa9d8fa1a1e31e847ce28c9179b
pete-ewg17 <peteakhurst-ewg@Petes-MacBook-Pro.local>
2017-08-28 15:50:21 +1000

    added latest-updates page

---
+++ latest-updates.php
@@ -0,0 +1,48 @@
+<?php
+$file_base = "";
+$title = "Welcome";
+$desc = "EWG Talent is a staffing agency like no other - one entirely tailored to the events, sport and entertainment industry";
+include $file_base."template/header.php";
+?>
+
+
+
+<!--Our Technology Advantage -->
+<div class="content-section-d">
+  <div class="container">
+    <div class="row">
+      <div data-aos="fade-left" data-aos-once="true" class="col-lg-5 col-lg-offset-1 col-sm-push-6 col-sm-6">
+        <h2 style="color: #555;">Our Technology Advantage</h2>
+        <p class="tech-lead" style="color: #555;">Our global leading, white labelled workforce management platform has enabled the likes of Super Bowl LI, Rugby League World Cup and Aspen Ski World Cup to easily schedule, train and fulfil accreditation needs for their entire workforce. Being involved with these major events gives us a blue print for the best practices for managing event staff. This directly improves our service, knowledge and expertise in staffing your event.</p>
+        <div class="button">
+          <a href="http://eventworkforcegroup.com/technology/" target="_blank" class="btn btn btn-outline">Learn More</a>
+        </div>
+      </div>
+      <div data-aos="fade-right" data-aos-once="true" class="col-lg-5 col-sm-pull-6 col-sm-6">
+        <img class="featurette-image img-responsive img-rounded" src="assets/images/tech_advantage.png" alt="Technology Advantage">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include $file_base.'template/footer.php';
+?>
